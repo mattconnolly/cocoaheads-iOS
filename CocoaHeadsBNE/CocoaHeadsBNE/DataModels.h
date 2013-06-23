e83f972275ea1a8d737dfde029daca8614079352
Darrin Mison <dmison@me.com>
2013-06-23 16:56:24 +1000

    added DataModel header for easy import, also grouped model and core data files
    Signed-off-by: Darrin Mison <dmison@me.com>

---
+++ CocoaHeadsBNE/CocoaHeadsBNE/DataModels.h
@@ -0,0 +1,12 @@
+//
+//  DataModel.h
+//  CocoaHeadsBNE
+//
+//  Created by tabby on 23/06/13.
+//  Copyright (c) 2013 CocoaHeads Brisbane. All rights reserved.
+//
+
+
+#import "Group.h"
+#import "Event.h"
+#import "Venue.h"
