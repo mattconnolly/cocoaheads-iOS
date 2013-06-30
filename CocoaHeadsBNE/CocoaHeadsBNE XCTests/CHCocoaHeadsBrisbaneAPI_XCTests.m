a8d7bf83071ab84eda8cc049c16b66ec2f12ec97
Darrin Mison <dmison@me.com>
2013-06-30 13:37:26 +1000

    refactored tests to better structure, added test for loading test json method
    Signed-off-by: Darrin Mison <dmison@me.com>

--- CocoaHeadsBNE/CocoaHeadsBNE XCTests/CHCocoaHeadsBrisbaneAPI_XCTests.m
+++ CocoaHeadsBNE/CocoaHeadsBNE XCTests/CHCocoaHeadsBrisbaneAPI_XCTests.m
@@ -10,6 +10,12 @@
 #import "CHCocoaHeadsBrisbaneAPI.h"
 
 @interface CHCocoaHeadsBrisbaneAPI_XCTests : XCTestCase
+{
+    NSManagedObjectContext *_context;
+    CHCocoaHeadsBrisbaneAPI *_api;
+    
+    NSDictionary *_eventJSON;
+}
 
 @end
 
@@ -20,6 +26,11 @@
     [super setUp];
     
     // Set-up code here.
+    _context = [[CHCoreData sharedInstance] newManagedObjectContext];
+    _api = [CHCocoaHeadsBrisbaneAPI sharedInstance];
+    
+    _eventJSON = [self loadSampleJSONFile:@"meetup-event.json"];
+    
 }
 
 - (void)tearDown
@@ -29,32 +40,49 @@
     [super tearDown];
 }
 
--(NSDictionary*)loadSampleEventJSON
+-(NSDictionary*)loadSampleJSONFile:(NSString*)filename
 {
+    filename = [filename stringByDeletingPathExtension];
+
     NSBundle *bundle = [NSBundle bundleForClass:[self class]];
-    NSString *url = [bundle pathForResource:@"meetup-event" ofType:@"json"];
+    NSString *url = [bundle pathForResource:filename ofType:@"json"];
     
     NSInputStream *is = [NSInputStream inputStreamWithFileAtPath:url];
-
     [is open];
     NSDictionary *dict = [NSJSONSerialization JSONObjectWithStream:is options:0 error:nil];
     [is close];
+    
     return dict;
 }
 
+#pragma mark - self tests
+- (void)testLoadSampleJSONFile
+{
+    //"created": 1320183473000,
+    //"time": 1364889600000,
+    //"visibility": "public",
+
+    NSDictionary *testData = [self loadSampleJSONFile:@"meetup-event.json"];
+    
+    NSNumber *created = (NSNumber*)[testData objectForKey:@"created"];
+    NSNumber *time = (NSNumber*)[testData objectForKey:@"time"];
+    NSString *visibility = (NSString*)[testData objectForKey:@"visibility"];
+    
+    XCTAssertTrue([created isEqualToNumber:[NSNumber numberWithDouble:1320183473000]], @"created value doesn't match");
+    XCTAssertTrue([time isEqualToNumber:[NSNumber numberWithDouble:1364889600000]], @"time value doesn't match");
+    XCTAssertTrue([visibility isEqualToString:@"public"], @"visibility value doesn't match");
+}
+
+#pragma mark - actual tests
+
 - (void)testGetVenueFromJSON
 {
-    CHCocoaHeadsBrisbaneAPI *api = [CHCocoaHeadsBrisbaneAPI sharedInstance];
-    CHCoreData *cd = [CHCoreData sharedInstance];
-    NSManagedObjectContext *context = [cd newManagedObjectContext];
-    
-    NSDictionary *json = [self loadSampleEventJSON];
-    
-    NSDictionary *venue = [json objectForKey:@"venue"];
-    Venue *v = (Venue*)[api getVenueFromJSON:venue withContext:context];
+    NSDictionary *venue = [_eventJSON objectForKey:@"venue"];
+    Venue *v = (Venue*)[_api getVenueFromJSON:venue withContext:_context];
     
     XCTAssertNotNil(v, @"venue was nil");
     XCTAssertTrue([v.name isEqualToString:@"River City Labs"], @"bad name: %@", v.name);
+    //TODO: add the remaining values to match
     
 }
 
