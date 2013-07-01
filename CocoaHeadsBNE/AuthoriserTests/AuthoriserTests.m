e15d4fea14fa8556b90749266e39553bced0af4c
Matt Connolly <matt.connolly@me.com>
2013-07-01 21:13:42 +1000

    Add test for CHPlistItem in Authoriser

--- CocoaHeadsBNE/AuthoriserTests/AuthoriserTests.m
+++ CocoaHeadsBNE/AuthoriserTests/AuthoriserTests.m
@@ -8,6 +8,7 @@
 
 #import "AuthoriserTests.h"
 #import "CHCrypto.h"
+#import "CHPlistItem.h"
 
 @implementation AuthoriserTests
 
@@ -37,6 +38,28 @@
     NSString* decrypted = [crypto decodeData:encrypted];
     XCTAssertNotNil(decrypted, @"decrypted data is not nil");
     XCTAssertEqualObjects(source, decrypted, @"decrypted string matches source string");
+}
+
+- (void)testCHPlistItem
+{
+    NSMutableDictionary* dict = [NSMutableDictionary dictionaryWithCapacity:1];
+    dict[@"key"] = @"Hello world!";
+    CHPlistItem* item = [[CHPlistItem alloc] initWithKey:@"key"
+                                            inDictionary:dict];
+    item.crypto = crypto;
+    
+    XCTAssertNotNil(item, @"item is not nil");
+    XCTAssertFalse(item.encrypted, @"item is not encrypted");
+    XCTAssertTrue([dict[@"key"] isKindOfClass:[NSString class]], @"item is a string");
+    XCTAssertTrue([item.value isKindOfClass:[NSString class]], @"item.value is a string");
+    item.encrypted = YES;
+    XCTAssertTrue(item.encrypted, @"item is encrypted");
+    XCTAssertTrue([dict[@"key"] isKindOfClass:[NSData class]], @"item is data");
+    XCTAssertTrue([item.value isKindOfClass:[NSString class]], @"item.value is a string");
+    item.encrypted = NO;
+    XCTAssertFalse(item.encrypted, @"item is not encrypted");
+    XCTAssertTrue([dict[@"key"] isKindOfClass:[NSString class]], @"item is a string");
+    XCTAssertTrue([item.value isKindOfClass:[NSString class]], @"item.value is a string");
 }
 
 @end
